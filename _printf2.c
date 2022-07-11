c073236d0953c04c2ae3f0f633367c6602413519
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-12 01:24:29 +0200

    Update _printf2.c

--- _printf2.c
+++ _printf2.c
@@ -4,13 +4,13 @@
 * @format: is a character string
 * Return: the number of characters printed
 */
-int _printf(const char* format, ...)
-{	
+int _printf(const char *format, ...)
+{
 	va_list list;
 	int j, i = 0, n = 0;
 
 	print printype[] = {{'c', printchar},
-						{'s', printstr}
+				{'s', printstr}
 	};
 
 	va_start(list, format);
