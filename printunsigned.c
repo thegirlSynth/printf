f2e2bdbbb2ad31f2ab6c78fcdaecce344939ab96
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-14 04:23:40 +0200

    Create printunsigned.c

---
+++ printunsigned.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* unsigned - handle u specifier in printf
+* @list: a va_list macro
+* Return: number of bytes printed
+*/
+int unsigned(va_list list)
+{
+	unsigned int n;
+	unsigned int num, div = 1, len = 0;
+
+	n = va_arg(list, unsigned int);
+	num = n;
+	while (num / div > 9)
+		div *= 10;
+	while (div != 0)
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+		return (len);
+}
