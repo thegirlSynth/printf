e21d96001ae9e64ffbea51ad7a9fe0e2c8312705
omniah arafah <105656911+omniah7@users.noreply.github.com>
2022-07-15 23:22:32 +0200

    Update and rename printstr.c to printSTR.c

---
+++ printSTR.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* printSRT - handles 
+* @list: a va_list macro
+* Return: number of bytes printed. 0 otherwise
+*/
+int printSTR(va_list list)
+{
+	int i, count = 0, n = 0;
+	char *s = va_arg(list, char *);
+
+	if (s == NULL)
+		s = "(null)";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 32 && s[i] < 127)
+			n += _putchar(s[i]);
+		else
+		{
+			n += _putchar('\\'):
+			n += _putchar('x');
+			if (s[i] < 16)
+			{
+				n += _putchar('0');
+			}
+			n += HEXA((unsigned int)s[i], &count);
+		}
+	}
+	return (n);
+}
+
