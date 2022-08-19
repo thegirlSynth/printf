d82157f5b76f19e70dc82a07e4269681d4d3a70b
Cynthia Uche <cynthianuchepro@gmail.com>
2022-08-19 14:13:22 -0700

    Update Script

--- printf.c
+++ printf.c
@@ -7,6 +7,27 @@
 
 int handle_specifiers()
 {
+	int i;
+	print printype[] = {
+		{'c', printchar},
+		{'s', printstr},
+		{'d', printnum},
+		{'i', printnum},
+		{'b', printbinary},
+		{'x', printhexa},
+		{'X', printHEXA},
+		{'o', printoctal},
+		{'u', printunsigned},
+		{'S', printSTR},
+		{'p', printSTR}
+	};
+	for (j = 0; j < 11; j++)
+	{
+		if (printype[j].t == format[i])
+			return (printype[j].f(list));
+	}
+
+	return (NULL);
 }
 
 
@@ -21,30 +42,21 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int val, j, i = 0, n = 0;
-	print printype[] = {{'c', printchar}, {'s', printstr}, {'d', printnum},
-		{'i', printnum}, {'b', printbinary}, {'x', printhexa},
-		{'X', printHEXA}, {'o', printoctal}, {'u', printunsigned},
-		{'S', printSTR}, {'p', printSTR}};
 
 	va_start(list, format);
 	if (format == NULL || (format[i] == '%' && format[i + 1] == '\0'))
 		return (-1);
+
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			for (j = 0; j < 11; j++)
-			{
-				if (printype[j].t == format[i])
-				{
-					val = printype[j].f(list);
-					if (val == -1)
-						return (-1);
-					n += val;
-					break;
-				}
-			}
+			val = printype[j].f(list);
+			if (val == -1)
+				return (-1);
+			n += val;
+
 			if (j == 11)
 			{
 				if (format[i] != '%')
